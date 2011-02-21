49ce11c8164547bf76fcc5081839a91731aa726e
Hanol Seo <hanol.seo@headflow.net>
2011-02-21 11:17:53 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IdeasWall::Application.config.secret_token = '7ce7f04d1f026c89f836ace6d21eba4c90fff232655351fc4061f0f960341fdfa455e95b51070051b34203d9c3643c91f863ec02ddc81a2640f1baec9f402b69'
